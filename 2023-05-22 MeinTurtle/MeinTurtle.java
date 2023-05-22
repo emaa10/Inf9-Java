7d69036457f889fb98b0d8ce1943756c1fddeb99
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2023-05-22 10:52:12 +0200

    move out

--- 2023-05-22 MeinTurtle/MeinTurtle.java
+++ 2023-05-22 MeinTurtle/MeinTurtle.java
@@ -10,18 +10,19 @@ public class MeinTurtle extends Turtle
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private int x;
 
+    @Override void Gehen(double länge) {
+        super.Gehen(länge);
+        this.Drehen(90);
+        super.Gehen(1);
+        this.Drehen(90);
+        super.Gehen(länge);
+    }
+    
     /**
      * Konstruktor für Objekte der Klasse MeinTurtle
      */
     public MeinTurtle()
     {
-        @Override void Gehen(double länge) {
-            super.Gehen(länge);
-            this.Drehen(90);
-            super.Gehen(1);
-            this.Drehen(90);
-            super.Gehen(länge);
-        }
     }
 
     /**
