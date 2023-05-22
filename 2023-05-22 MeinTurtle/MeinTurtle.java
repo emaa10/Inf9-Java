1c10200fc6456441b74771daab6ed35672e4c5d2
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2023-05-22 11:06:11 +0200

    d

--- 2023-05-22 MeinTurtle/MeinTurtle.java
+++ 2023-05-22 MeinTurtle/MeinTurtle.java
@@ -31,16 +31,4 @@ public class MeinTurtle extends Turtle
         super();
         wegstrecke = 0.0;
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
 }
