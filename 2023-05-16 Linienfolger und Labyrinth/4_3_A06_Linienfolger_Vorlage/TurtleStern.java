043a12b907222be7b9640c44320d6e430ffd8318
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2023-06-12 08:37:33 +0000

    adding files

---
+++ 2023-05-16 Linienfolger und Labyrinth/4_3_A06_Linienfolger_Vorlage/TurtleStern.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse TurtleStern.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class TurtleStern extends Turtle
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse TurtleStern
+     */
+    public TurtleStern()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
