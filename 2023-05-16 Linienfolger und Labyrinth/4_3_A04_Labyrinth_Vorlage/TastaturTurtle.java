cc3d6ed984b2b06135fb836a78f418ae475b5a0a
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2023-06-13 10:25:17 +0000

    add new mode

--- 2023-05-16 Linienfolger und Labyrinth/4_3_A04_Labyrinth_Vorlage/TastaturTurtle.java
+++ 2023-05-16 Linienfolger und Labyrinth/4_3_A04_Labyrinth_Vorlage/TastaturTurtle.java
@@ -7,26 +7,53 @@
  */
 public class TastaturTurtle extends Turtle
 {
+    public int mode = 1;    //1: normal, 2: save angles
+    public double currentAngle = 90;
+
     @Override void SonderTasteGedrückt(int taste) {
-        System.out.println("Sondertaste: " + taste);
-        // Gehen(10);
-        switch(taste) {
-            case 38:
-                Gehen(10);
-                break;
-            case 39:
-                Drehen(90);
-                break;
-            case 40:
-                Drehen(180);
-                break;
-            case 37:
-                Drehen(-90);
-                break;
-                
+        if(mode == 1) {
+            System.out.println("Sondertaste: " + taste);
+            // Gehen(10);
+            switch(taste) {
+                case 38:
+                    Gehen(10);
+                    break;
+                case 39:
+                    Drehen(90);
+                    break;
+                case 40:
+                    Drehen(180);
+                    break;
+                case 37:
+                    Drehen(-90);
+                    break;
+                    
+            }
+        }
+        else {
+            switch(taste) {
+                case 38:    //pfeil hoch
+                    Drehen(dreheZu(0));
+                    break;
+                case 39:    //pfeil rechts
+                    Drehen(dreheZu(90));
+                    break;
+                case 40:    //pfeil runter
+                    Drehen(dreheZu(180));
+                    break;
+                case 37:    //pfeil links
+                    Drehen(dreheZu(-90));
+                    break;
+                    
+            }
+            Gehen(10);
         }
     }
 
+    public int dreheZu(double angle) {
+        double turn = angle - currentAngle;
+        return turn;
+    }
 
     @Override void MausGeklickt(int x, int y, int anzahl) {
         PositionSetzen(x, y);
