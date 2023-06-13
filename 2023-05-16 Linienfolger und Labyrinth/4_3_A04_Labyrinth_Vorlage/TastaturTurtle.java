d5d58e3f93c101c6771d3eb3ac1975ad6d8b8997
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2023-06-13 09:59:16 +0000

    adding new classes

---
+++ 2023-05-16 Linienfolger und Labyrinth/4_3_A04_Labyrinth_Vorlage/TastaturTurtle.java
@@ -0,0 +1,11 @@
+
+/**
+ * Beschreiben Sie hier die Klasse TastaturTurtle.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class TastaturTurtle extends Turtle
+{
+
+}
