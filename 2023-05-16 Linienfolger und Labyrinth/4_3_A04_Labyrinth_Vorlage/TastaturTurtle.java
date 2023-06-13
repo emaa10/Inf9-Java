dbcd2c1b5df8195b471ad1766a2f84b8aa678b5b
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2023-06-13 10:04:26 +0000

    133/5b

--- 2023-05-16 Linienfolger und Labyrinth/4_3_A04_Labyrinth_Vorlage/TastaturTurtle.java
+++ 2023-05-16 Linienfolger und Labyrinth/4_3_A04_Labyrinth_Vorlage/TastaturTurtle.java
@@ -10,6 +10,21 @@ public class TastaturTurtle extends Turtle
     @Override void SonderTasteGedrückt(int taste) {
         System.out.println("Sondertaste: " + taste);
         Gehen(10);
+        switch(taste) {
+            case 37:
+                Gehen(10);
+                break;
+            case 38:
+                Drehen(90);
+                break;
+            case 39:
+                Drehen(180);
+                break;
+            case 40:
+                Drehen(-90);
+                break;
+        }
     }
+
 
 }
